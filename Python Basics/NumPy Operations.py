6ece1b876617e47b14961080551e98b8ef8abf8b
Iris <44437616+limit-DNE@users.noreply.github.com>
2020-04-20 23:32:20 +0300

    Add files via upload

---
+++ Python Basics/NumPy Operations.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import numpy as np
+
+#Arithmetics
+arr = np.arange(0,10)
+arr
+
+
+# In[2]:
+
+
+arr + arr
+
+
+# In[3]:
+
+
+arr * arr
+
+
+# In[5]:
+
+
+arr - arr
+
+
+# In[6]:
+
+
+arr**3
+
+
+# In[7]:
+
+
+#Universal Array Functions
+np.sqrt(arr)
+
+
+# In[8]:
+
+
+np.exp(arr)#Calcualting exponential (e^)
+
+
+# In[9]:
+
+
+np.max(arr) #same as arr.max()
+
+
+# In[10]:
+
+
+np.sin(arr)
+
+
+# In[ ]:
+
+
+
+
